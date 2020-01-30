6e82533a9ad10ed23d5586db61773a3bb378f7ad
Gabriel <gabriel.campos9815@gmail.com>
2020-01-30 16:19:11 -0300

    using yarn in the frontend project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,44 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  state = {
-    response: '',
-    user: '',
-    details:'',
-  };
-
-  callApi = async () => {
-    console.log(this.state.user);
-    const response = await fetch(`https://api.github.com/users/${this.state.user}/repos`);
-    const body = await response.json();
-    if (response.status !== 200) throw Error(body.message);
-
-
-    this.setState({
-      details: body[0].owner
-    });
-  }
-  changeField(fieldName, event) {
-    var field = {};
-    field[fieldName] = event.target.value;
-    this.setState(field);
-  }
-
-  render() {
-    return (
-      <div>
-        <form>
-          <input value={this.state.user} name="user" id="user" type="text" onChange={this.changeField.bind(this, 'user')} />
-          <div className="App">
-            <p className="App-intro">{this.state.response}</p>
-          </div>
-          <button type="button" onClick={this.callApi}>Conectar</button>
-        </form>
-        <p>{this.state.details}</p>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
