f07a6f7d9063bf3ab529df98f755b024ab43afe1
Gabriel <gabriel.campos9815@gmail.com>
2020-01-30 13:25:30 -0300

    backend and some routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,19 +1,17 @@
 const express = require('express');
 const routes = express.Router();
 const fetch = require("node-fetch");
-const client_id = "Iv1.581d4b5fb4dc34b5";
-const client_secret = "e02b66a95c206ca5e24bf8f42fb9238cd7229a79";
 
+routes.get('/api/users?since=:number', (req,res)=>{
 
-routes.get('/api/users?since={number}', (req,res)=>{
     res.send({status:'deu certo'});
 });
 routes.get('/api/users/:username/details', async (req,res)=>{
     console.log(req.params);
-    return  res.json(await fetch(`https://api.github.com/users/${req.params.username}?client_id=${client_id}&client_secret=${client_secret}`).then((response)=>response.json()));
+    return  res.json(await fetch(`https://api.github.com/users/${req.params.username}`).then((response)=>response.json()));
 });
 routes.get('/api/users/:username/repos', async (req,res)=>{
-    return  res.json(await fetch(`https://api.github.com/users/${req.params.username}/repos?client_id=${client_id}&client_secret=${client_secret}`).then((response)=>response.json()));
+    return  res.json(await fetch(`https://api.github.com/users/${req.params.username}/repos`).then((response)=>response.json()));
 });
 
 module.exports = routes;
