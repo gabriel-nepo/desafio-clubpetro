233c77c12637a07f6281f122555c8465e9e000b4
Gabriel <gabriel.campos9815@gmail.com>
2020-01-31 12:50:32 -0300

    fixes and remove of repositories component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,6 @@ import { Navbar, Nav } from 'react-bootstrap';
 import './App.css';
 import UsersTable from './components/UsersTable';
 import Details from './components/Details';
-import Repositories from './components/Repositories';
 
 export default class App extends Component {
   constructor() {
@@ -30,14 +29,11 @@ export default class App extends Component {
     else if (this.state.active === 'details') {
       return <Details />;
     }
-    else if (this.state.active === 'repositories') {
-      return <Repositories />
-    }
     else {
       return (
         <div>
-          <img className="homepage" src={logo} alt="Detective Octocat"></img>
-          <table className="table table-striped">
+          <img className="homepage" src={logo} alt="Detective Octocat" width="30%"></img>
+          <table align="center" className="table table-striped">
             <thead align="center">
               <tr>
                 <th>Guide</th>
@@ -48,10 +44,7 @@ export default class App extends Component {
                 <td>Users - List of GitHub users (ID,login)</td>
               </tr>
               <tr align="center">
-                <td>Public Repositories - Search for a user and get his public repositories (ID,Name,URL)</td>
-              </tr>
-              <tr align="center">
-                <td>Details - Search for a user and get his details (ID,Login,URL,Created At)</td>
+                <td>Details - Search for a user and get his details (ID,Login,URL,Created At) and his public repositories (ID,Name,URL)</td>
               </tr>
             </tbody>
           </table>
@@ -63,12 +56,11 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <Navbar bg="dark" variant="dark">
+        <Navbar style={{width:"100%"}} bg="dark" variant="dark">
           <img src={logo} alt="Detective Octocat" height="42" width="42"></img>
           <Navbar.Brand href="#home" name="home" id="home" onClick={this.handleClick.bind(this)}>Detective Octocat</Navbar.Brand>
           <Nav className="mr-auto">
             <Nav.Link id="users" name="users" href="#users" onClick={this.handleClick.bind(this)}>Users</Nav.Link>
-            <Nav.Link id="repositories" name="repositories" href="#repositories" onClick={this.handleClick.bind(this)}>Public Repositories</Nav.Link>
             <Nav.Link id="details" name="details" href="#details" onClick={this.handleClick.bind(this)}>Details</Nav.Link>
           </Nav>
         </Navbar>
--- frontend/src/components/Details.js
+++ frontend/src/components/Details.js
@@ -13,7 +13,8 @@ export default class Details extends Component {
         this.state = {
             user: "",
             data: [],
-            message: 'Search'
+            message: 'Search for a username',
+            repo: []
         };
     }
     changeField(fieldName, event) {
@@ -23,18 +24,36 @@ export default class Details extends Component {
     }
 
     async handleSearch() {
-        let res = await fetch(`${baseUrl}api/users/${this.state.user}/details`).then((response) => {
+        let details = await fetch(`${baseUrl}api/users/${this.state.user}/details`).then((response) => {
             return response.json();
         });
-        if(res.message !=='Not Found'){
-            this.setState({
-                data: [{
-                    id : res.id,
-                    login: res.login,
-                    url: res.html_url,
-                    created_at: new Date(res.created_at)
-                }]
+
+        if (details.message !== 'Not Found') {
+            let repos = await fetch(`${baseUrl}api/users/${this.state.user}/repos`).then((response) => {
+                return response.json();
             });
+            let array_repos = repos.map(repo => {
+                return {
+                    id: repo.id,
+                    name: repo.name,
+                    url: repo.html_url,
+                }
+            })
+            let user_info = [{
+                id: details.id,
+                login: details.login,
+                url: details.html_url,
+                created_at: new Date(details.created_at)
+
+            }]
+            console.log(user_info);
+            console.log(array_repos);
+
+            this.setState({
+                data: user_info,
+                repo: array_repos
+            });
+            console.log(this.state.data);
         }
         this.setState({
             message: 'Not Found'
@@ -44,7 +63,7 @@ export default class Details extends Component {
     render() {
         return (
             <div>
-                <h1 className="center page-header">User Details</h1>
+                <h1 className="center sub-header">User Details</h1>
                 <div className="rt-margin">
                     <div className="mb-3 input-group">
                         <input value={this.state.user} id="user" name="user" placeholder="Search for a username to get his details" aria-label="Search for a username to get his details" aria-describedby="basic-addon2" className="form-control" onChange={this.changeField.bind(this, 'user')} />
@@ -75,7 +94,7 @@ export default class Details extends Component {
                             {
                                 Header: "URL",
                                 accessor: "url",
-                            Cell: row => <div style={{ textAlign: "center" }}>{<a href={row.value} target="_blank" rel="noopener noreferrer">{row.value}</a>}</div>
+                                Cell: row => <div style={{ textAlign: "center" }}>{<a href={row.value} target="_blank" rel="noopener noreferrer">{row.value}</a>}</div>
                             },
                             {
                                 Header: "Creation At",
@@ -86,6 +105,39 @@ export default class Details extends Component {
                         className="-striped -highlight"
                         defaultPageSize={1}
                     />
+
+                </div>
+                <div className="margin-bottom">
+                    <h1 className="center page-header">User Repositories</h1>
+                    <div className="rt-margin">
+                        
+                        <ReactTable
+                            showPageSizeOptions={false}
+                            showPageJump={false}
+                            data={this.state.repo}
+                            noDataText={this.state.message}
+
+                            columns={[
+                                {
+                                    Header: "ID",
+                                    accessor: "id",
+                                    Cell: row => <div style={{ textAlign: "center" }}>{row.value}</div>
+                                },
+                                {
+                                    Header: "Name",
+                                    accessor: "name",
+                                    Cell: row => <div style={{ textAlign: "center" }}>{row.value}</div>
+                                },
+                                {
+                                    Header: "URL",
+                                    accessor: "url",
+                                    Cell: row => <div style={{ textAlign: "center" }}>{<a href={row.value} target="_blank" rel="noopener noreferrer">{row.value}</a>}</div>
+                                }
+                            ]}
+                            className="-striped -highlight"
+                            defaultPageSize={10}
+                        />
+                    </div>
                 </div>
             </div>
         );
